ee498c329c49f218fbbc64c1fee26607d2122804
Steve Lemuel <wlemuel@hotmail.com>
2019-11-08 15:17:55 +0800

    feat: 添加 NotifyView 页面

---
+++ readhub/Common/Components/NotifyView.swift
@@ -0,0 +1,14 @@
+//
+//  NotifyView.swift
+//  readhub
+//
+//  Created by Steve Lemuel on 11/8/19.
+//  Copyright © 2019 Steve Lemuel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NotifyView: UIView {
+    
+}
