af479d60d31cdf5474a2e880bdc4372f4ab3b345
Steve Lemuel <wlemuel@hotmail.com>
2019-11-08 11:31:11 +0800

    style: change primary color of light mode

--- readhub/Modules/Home/HomeViewController.swift
+++ readhub/Modules/Home/HomeViewController.swift
@@ -16,13 +16,10 @@ import TYPagerController
 import UIKit
 
 fileprivate struct Metric {
-    static let pagerBarFont = UIFont.systemFont(ofSize: 18.0)
+    static let pagerBarFontSize: CGFloat = 18.0
     static let pagerBarHeight: CGFloat = 40.0
 
-    static let titleHeight: CGFloat = 50.0
-
-    static let titleIconSize: CGFloat = 25.0
-    static let titleIconMargin: CGFloat = 15.0
+    static let logoFontSize: CGFloat = 25.0
 }
 
 final class HomeViewController: BaseViewController {
@@ -37,21 +34,9 @@ final class HomeViewController: BaseViewController {
     private var titleView: UIView!
     private var settingBtn: UIImageView!
     private var bookmarkBtn: UIButton!
+    private var pageVC: TYTabPagerController!
 
     private let titles: [String] = ["热门话题", "科技动态", "开发者", "区块链"]
-    private let pageVC = TYTabPagerController().then {
-        $0.pagerController.layout.prefetchItemCount = 3
-        $0.tabBar.layout.cellWidth = kScreenW * 0.24
-        $0.tabBar.layout.progressColor = kThemePrimaryColor
-        $0.tabBar.layout.selectedTextColor = kThemePrimaryColor
-        $0.tabBar.backgroundColor = kThemeBase2Color
-        $0.tabBar.layout.cellSpacing = 0
-        $0.tabBar.layout.cellEdging = 0
-        $0.tabBar.layout.normalTextFont = Metric.pagerBarFont
-        $0.tabBar.layout.selectedTextFont = Metric.pagerBarFont
-        $0.tabBar.layout.adjustContentCellsCenter = true
-        $0.tabBarHeight = Metric.pagerBarHeight
-    }
 
     // MARK: - Lifecycle -
 
@@ -66,29 +51,59 @@ final class HomeViewController: BaseViewController {
         super.viewWillAppear(animated)
         setupNavigationBar()
     }
+}
 
-    private func setupNavigationBar() {
+// MARK: - Extensions -
+
+extension HomeViewController: HomeViewInterface {
+}
+
+private extension HomeViewController {
+    func configure() {
+        let output = Home.ViewOutput()
+
+        _ = presenter.configure(with: output)
+    }
+
+    func setupNavigationBar() {
         navigationController?.navigationBar.barTintColor = kThemeBase2Color
     }
 
-    private func setupLayout() {
+    func setupLayout() {
         setupTitleView()
         setupPageController()
     }
 
-    private func setupTitleView() {
+    func setupTitleView() {
         let logo = UIBarButtonItem(title: "Readhub+", style: .plain, target: self, action: nil).then {
             let attributes = [
                 NSAttributedString.Key.foregroundColor: kThemePrimaryColor,
-                NSAttributedString.Key.font: UIFont.systemFont(ofSize: Metric.titleIconSize),
+                NSAttributedString.Key.font: UIFont.systemFont(ofSize: Metric.logoFontSize),
             ]
             $0.setTitleTextAttributes(attributes, for: .normal)
         }
 
         navigationItem.leftBarButtonItem = logo
+
+        let settings = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(gotoSettings))
+        navigationItem.rightBarButtonItem = settings
     }
 
-    private func setupPageController() {
+    func setupPageController() {
+        pageVC = TYTabPagerController().then {
+            $0.pagerController.layout.prefetchItemCount = 3
+            $0.tabBar.layout.cellWidth = kScreenW * 0.24
+            $0.tabBar.layout.progressColor = kThemePrimaryColor
+            $0.tabBar.layout.selectedTextColor = kThemePrimaryColor
+            $0.tabBar.backgroundColor = kThemeBase2Color
+            $0.tabBar.layout.cellSpacing = 0
+            $0.tabBar.layout.cellEdging = 0
+            $0.tabBar.layout.normalTextFont = UIFont.systemFont(ofSize: Metric.pagerBarFontSize)
+            $0.tabBar.layout.selectedTextFont = UIFont.systemFont(ofSize: Metric.pagerBarFontSize)
+            $0.tabBar.layout.adjustContentCellsCenter = true
+            $0.tabBarHeight = Metric.pagerBarHeight
+        }
+
         addChild(pageVC)
         view.addSubview(pageVC.view)
 
@@ -109,20 +124,11 @@ final class HomeViewController: BaseViewController {
         pageVC.pagerController.scrollToController(at: 0, animate: false)
     }
 
-    private func setRx() {
+    func setRx() {
     }
-}
 
-// MARK: - Extensions -
-
-extension HomeViewController: HomeViewInterface {
-}
-
-private extension HomeViewController {
-    func configure() {
-        let output = Home.ViewOutput()
-
-        _ = presenter.configure(with: output)
+    @objc func gotoSettings() {
+        navigationController?.pushWireframe(SettingsWireframe())
     }
 }
 
