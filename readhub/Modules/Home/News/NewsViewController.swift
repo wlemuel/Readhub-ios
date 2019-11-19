4aed51bb4999cc572c406b0e4fcff1a4be657e23
Steve Lemuel <wlemuel@hotmail.com>
2019-11-19 13:41:19 +0800

    optimize: update logic for lastCursor and lastReadMark

--- readhub/Modules/Home/News/NewsViewController.swift
+++ readhub/Modules/Home/News/NewsViewController.swift
@@ -132,22 +132,14 @@ class NewsViewController: BaseViewController {
 
         // update lastcursor
         dataDriver.filter { $0.count > 0 }
-            .map { $0.last }
+            .map { ($0.first, $0.last) }
             .asObservable()
-            .subscribe(onNext: { [weak self] item in
-                guard let `self` = self else { return }
+            .subscribe(onNext: { [weak self] pair in
+                guard let `self` = self, let first = pair.0, let last = pair.1,
+                    let firstId = first.id else { return }
 
-                self.lastCursor = item?.publishDate?.toUnixMillTime() ?? ""
-            }).disposed(by: disposeBag)
-
-        // update lastreadmark
-        dataDriver.filter { $0.count > 0 }
-            .map { $0.first }
-            .asObservable()
-            .subscribe(onNext: { [weak self] item in
-                guard let `self` = self, let id = item?.id else { return }
-
-                self.tempReadMark = id
+                self.lastCursor = last.publishDate?.toUnixMillTime() ?? ""
+                self.tempReadMark = firstId
             }).disposed(by: disposeBag)
 
         errorDriver = presenter.errors.asDriver(onErrorJustReturn: .serverFailed(newsType: .unknown))
